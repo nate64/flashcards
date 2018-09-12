cfcced2a6bedf4f0db878b12e1e8b229c7664f08
Nate <nathanestes@gmail.com>
2018-09-12 17:03:47 -0600

    materialize

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready( function() {
+
+
+const = big array{}
+
+jquery onclick to flip card, reveal answer
+-put in actual JS knowledges
+
+
+});
+
+
+
+
+
+//need flashcard material - array of key/values pairs? (front & back)
+//randomly select from array to present set number of cards to user
+//user can edit
+//user can delete
+//user can add
